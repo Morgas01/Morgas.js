2004f3e9fbbfce5c57479e36b04ffc3b2b77abbe
Morgas <morgas01@web.de>
2014-11-05 14:36:36 +0100

    Dependencies

---
+++ src/Morgas.Dependencies.js
@@ -0,0 +1,17 @@
+(function(µ,SMOD,GMOD){
+    var DepRes=GMOD("DepRes");
+    µ.dependencies=new DepRes({
+        "Morgas.js":true,
+        "Morgas.DependencyResolver.js":"Morgas.js",
+        "Morgas.NodePatch.js":"Morgas.js",
+        "Morgas.util.object.js":"Morgas.js",
+        "Morgas.util.crc.js":"Morgas.js",
+        "Morgas.Organizer.js":{deps:["Morgas.js"],uses:["Morgas.util.object.js"]},
+        "Morgas.Detached.js":"Morgas.js",
+
+        "DB/Morgas.DB.js":{deps:["Morgas.js"],uses:["Morgas.Detached.js"]},
+        "DB/Morgas.DB.ObjectConnector.js":{deps:["Morgas.js","DB/Morgas.DB.js","Morgas.Organizer.js"],uses:["Morgas.util.object.js"]},
+        "DB/Morgas.DB.IndexedDBConnector.js":{deps:["Morgas.js","DB/Morgas.DB.js"],uses:["Morgas.Detached.js","Morgas.util.object.js"]},
+        "DB/Morgas.Organizer.LazyCache.js":{deps:["Morgas.js","Morgas.Organizer.js"],uses:["Morgas.Detached.js","Morgas.util.object.js"]}
+    });
+})(Morgas,Morgas.setModule,Morgas.getModule);
--- src/Morgas.DependencyResolver.js
+++ src/Morgas.DependencyResolver.js
@@ -15,46 +15,60 @@
 					var k=keys[i];
 					if(this.config[k]===undefined||overwrite)
 					{
-						this.config[k]=obj[k]
+                        if(typeof obj[k]==="string")
+                        {
+                            this.config[k]={deps:[obj[k]],uses:[]};
+                        }
+                        else if (Array.isArray(obj[k]))
+                        {
+                            this.config[k]={deps:obj[k].slice(),uses:[]};
+                        }
+                        else if (obj[k]!==true)
+                        {
+                            this.config[k]={deps:(obj[k].deps||[]).slice(),uses:(obj[k].uses||[]).slice()}
+                        }
+                        else
+                        {
+                            this.config[k]=true;
+                        }
 					}
 				}
 				return true;
 			}
-			µ.debug("DependencyResolver.setConfig: obj is not an object", 0);
+			µ.debug("DependencyResolver.addConfig: obj is not an object", 0);
 			return false;
 		},
 		resolve:function(items)
 		{
-			var rtn=[],
-			list=[].concat(items);
-			items=[].concat(items);
+			var rtn=[], list=[].concat(items), items=[].concat(items);
 			while(list.length>0)
 			{
-				var	resolved=true,
-				conf=this.config[list[0]];
+				var	resolved=true,conf=this.config[list[0]];
 				if(conf===undefined)
 				{
 					µ.debug("DependencyResolver.resolve: "+list[0]+" is undefined", 2);
 				}
-				else if(conf!==true&&(typeof conf!=="object"||Array.isArray(conf)||conf.deps!==undefined))
+				else if(conf!==true)
 				{
-					var deps=conf;
-					if(typeof deps==="object"&&!Array.isArray(conf))
-					{
-						deps=deps.deps;
-					}
-					var deps=[].concat(deps);
+					var deps=conf.deps;
+                    for(var i=0;i<conf.uses.length;i++)
+                    {
+                        if(list.indexOf(conf.uses[i])===-1)
+                        {
+                            list.push(conf.uses[i]);
+                        }
+                    }
 					for(var i=0;i<deps.length;i++)
 					{
 						var dep=deps[i];
 						if(rtn.indexOf(dep)===-1)
-						{
+						{//not yet depending
 							var listIndex=list.indexOf(dep);
 							if(listIndex!==-1)
-							{
+							{//as remaining item
 								
 								if(items.indexOf(dep)===-1)
-								{
+								{//not as item
 									throw new TypeError("cyclic object Dependencies ["+list[0]+","+deps[i]+"]");
 								}
 								else
@@ -74,7 +88,21 @@
 				}
 			}
 			return rtn;
-		}
+		},
+        clone:function(prefix)
+        {
+            var config=null;
+            if(prefix)
+            {
+                config={};
+                var mapFn=function(v){return prefix+v};
+                for(var i in this.config)
+                {
+                    config[prefix+i]=(this.config[i]===true ? true : {deps:this.config[i].deps.map(mapFn),uses:this.config[i].uses.map(mapFn)})
+                }
+            }
+            return new µ.DependencyResolver(config);
+        }
 	});
 	SMOD("DepRes",µ.DependencyResolver);
 	
