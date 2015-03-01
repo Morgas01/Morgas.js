76c714d88c3e3d50a540542875ecbde6e25d4d71
Morgas <morgas01@web.de>
2015-03-01 16:35:37 +0100

    build

--- build/Morgas.util.object.goPath.js
+++ build/Morgas.util.object.goPath.js
@@ -1,1 +1,1 @@
-(function(e,t){var n=e.util=e.util||{},i=n.object||{};i.goPath=function(e,t){var n=t;for("string"==typeof n&&(n=n.split("."));n.length>0&&e;)e=e[n.shift()];return n.length>0?void 0:e},t("goPath",i.goPath)})(Morgas,Morgas.setModule,Morgas.getModule);
+(function(e,t){var n=e.util=e.util||{},i=n.object||{};i.goPath=function(e,t,n){var i=t;for("string"==typeof i&&(i=i.split("."));i.length>0&&e;)!n||i[0]in e||(e[i[0]]={}),e=e[i.shift()];return i.length>0?void 0:e},t("goPath",i.goPath)})(Morgas,Morgas.setModule,Morgas.getModule);
