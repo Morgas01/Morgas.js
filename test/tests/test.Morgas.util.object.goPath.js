4df29309d30ccea43a825c1b6c858ac88305843a
Morgas <morgas01@web.de>
2015-08-28 11:28:41 +0200

    goPath.guide test

--- test/tests/test.Morgas.util.object.goPath.js
+++ test/tests/test.Morgas.util.object.goPath.js
@@ -20,5 +20,17 @@
 		ok(Array.isArray(obj.path.to["new"].arr),"create array");
 		strictEqual(goPath(obj,["path","to","arr[1]"],true),obj.path.to.arr[1],"array notation");
 	});
+
+	test("goPath.guide",function()
+	{
+		var obj={path:{to:{
+			value:"something",
+			"other value":"something else",
+			arr:["something1","something2"]
+		}}};
+		strictEqual(goPath.guide("path.to.value")(obj),obj.path.to.value,"valid path");
+		strictEqual(goPath.guide("path.to.no.value")(obj),undefined,"nonvalid path");
+		strictEqual(goPath.guide(["path","to","other value"])(obj),obj.path.to["other value"],"valid path as array");
+	});
 	
 })(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
