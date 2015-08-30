b44bf78a9bfa3c826aed609d7d9a7a7dde7e8015
Morgas <morgas01@web.de>
2015-08-31 01:43:03 +0200

    object connector fix start

--- src/DB/Morgas.DB.ObjectConnector.js
+++ src/DB/Morgas.DB.ObjectConnector.js
@@ -14,23 +14,30 @@
 		find:"find"
 	});
 	
-	var OCON;
-	
-	OCON=DBC.ObjectConnector=µ.Class(DBC,
+	var getDb=function()
 	{
-		db:new ORG().group("objectType","objectType"),
+		return new ORG().group("objectType","objectType",function(tDb)
+		{
+			tDb.map("id","fields.id.value");
+		});
+	}
+	
+	var OCON=DBC.ObjectConnector=µ.Class(DBC,
+	{
+		db:getDb(),
 		init:function(local)
 		{
 			this.mega();
 			if(!local)
 			{
-				this.db=new ORG().group("objectType","objectType");
+				this.db=getDb();
 			}
 		},
 		save:function(signal,objs)
 		{
 			objs=[].concat(objs);
 			var sortedObjs=DBC.sortObjs(objs);
+			
 			for(var objectType in sortedObjs.fresh)
 			{
 				var objs=sortedObjs.fresh[objectType],
@@ -42,20 +49,23 @@
 					this.db.add([{objectType:objs[i].objectType,fields:objs[i].toJSON()}]);
 				}
 			}
-
+			
+			var updates=[];
 			for(var objectType in sortedObjs.preserved)
 			{
 				var objs=sortedObjs.preserved[objectType],
-				group=this.db.getGroupValue("objectType",objectType);
+				ids=this.db.getGroupPart("objectType",objectType).getMap("id");
 				for(var i=0;i<objs.length;i++)
 				{
-					var found=SC.find(group,{fields:{ID:objs[i].getID()}});
-					if(found.length>0)
+					var found=ids[objs[i].getID()];
+					if(found)
 					{
-						found[0].value.fields=objs[i].toJSON();
+						found.value.fields=objs[i].toJSON();
+						updates.push(found)
 					}
 				}
 			}
+			this.db.update(updates);
 
 			for(var objectType in sortedObjs.friend)
 			{
@@ -76,44 +86,38 @@
 			}
 			signal.resolve();
 		},
-		load:function(signal,objClass,pattern,sort,DESC)
+		load:function(signal,objClass,pattern,sort)
 		{
-			var values=this.db.getGroupValue("objectType",objClass.prototype.objectType),
-			rtn=[];
+			var tDb=this.db.getGroupPart("objectType",objClass.prototype.objectType);
+			
+			var patternKey=null;
+			switch(typeof pattern)
+			{
+				case "string":
+					patternKey=pattern;
+					break;
+				case "object":
+					patternkey=JSON.stringify(pattern);
+					break;
+				case "function":
+					patternKey=pattern
+			}
+			if(!tDb.hasFilter(patternKey)) tDb.filter(pattnerKey,pattern);
+			var pDb=tDb.getFilter(patternKey);
 			
 			if(sort)
 			{
-				sort=ORG.pathSort("fields."+sort+".value",DESC);
+				sort=[].concat(sort).map(s=>"fields."+s+".value");
+				var sortKey=JSON.stringify(sort);
+				if(!pDb.hasSort(sortKey)) pDb.sort(sortKey,ORG.attributeSort(sort));
+				signal.resolve(pDb.getSort(sortKey));
 			}
-			
-			for(var i=0;i<values.length;i++)
-			{
-				if(SC.eq(values[i].fields,pattern))
-				{
-					var instance=new objClass();
-					instance.fromJSON(values[i].fields);
-					if(sort)
-					{
-						rtn.splice(ORG.getOrderIndex(instance,rtn,sort),0,instance);
-					}
-					else
-					{
-						rtn.push(instance);
-					}
-				}
-			}
-			signal.resolve(rtn);
+			else signal.resolve(pDb.getValues());
 		},
 		"delete":function(signal,objClass,toDelete)
 		{
-			toDelete={objectType:objClass.prototype.objectType,fields:DBC.getDeletePattern(objClass,toDelete)};
-			var filterKey=JSON.stringify(toDelete),
-			values=this.db.filter(filterKey,toDelete).getFilter(filterKey);
-			for(var i=0;i<values.length;i++)
-			{
-				this.db.remove(values[i]);
-			}
-			this.db.removeFilter(filterKey);
+			toDelete=SC.find(this.db.values,{objectType:objClass.prototype.objectType,fields:DBC.getDeletePattern(objClass,toDelete)},true);
+			this.db.remove(toDelete);
 			signal.resolve();
 		},
 		destroy:function()
@@ -128,19 +132,15 @@
 		_getNextID:function(objectType)
 		{
 			var rtn=[],
-			group=this.db.getGroupValue("objectType",objectType);
+			tDb=this.db.getGroupPart("objectType",objectType);
+			if(!tDb)return [0];
+			var ids=Object.keys(tDb.getIndexMap("id"));
 			var i=0;
-			for(;group.length>0;i++)
+			for(;ids.length>0;i++)
 			{
-				var found=SC.find(group,{fields:{ID:i}});
-				if(found.length===0)
-				{
-					rtn.push(i);
-				}
-				else
-				{
-					group.splice(found[0].index,1);
-				}
+				var index=ids.indexOf(i);
+				if(index===-1) rtn.push(i);
+				else group.splice(index,1);
 			}
 			rtn.push(i);
 			return rtn;
--- test/imports.js
+++ test/imports.js
@@ -69,7 +69,7 @@
 
 	WR(srcDir+"Morgas.Organizer.js");
 	WR(testDir+"test.Morgas.Organizer.js");
-/*
+
 	WR(srcDir+"Morgas.NodePatch.js");
 	WR(testDir+"test.Morgas.NodePatch.js");
 	
@@ -86,6 +86,6 @@
 
 	WR(srcDir+"DB/Morgas.DB.IndexedDBConnector.js");
 	WR(testDir+"test.Morgas.DB.IndexedDBConnector.js");
-	*/
+	
 
 })();
