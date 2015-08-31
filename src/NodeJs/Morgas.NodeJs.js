01185f8da9f958bbd87b3178e7fca2da851de0ce
Morgas <morgas01@web.de>
2015-08-31 14:58:50 +0200

    remove dependencyResolver

--- src/NodeJs/Morgas.NodeJs.js
+++ src/NodeJs/Morgas.NodeJs.js
@@ -1,28 +1,13 @@
 var path=require("path");
 require(path.join("..","Morgas"));
-require(path.join("..","Morgas.DependencyResolver"));
 
 var moduleRegister =	require("../Morgas.ModuleRegister");
-var dependencies =		require("../Morgas.Dependencies.json");
-dependencies["Morgas.DependencyResolver.js"]=true;
-var depRes =			new µ.DependencyResolver(dependencies);
 
 var oldGetModule=µ.getModule;
 
 µ.getModule=function(key)
 {
 	if(!µ.hasModule(key) && key in moduleRegister)
-	{
-		var files=depRes.resolve(moduleRegister[key]);
-		for(var i=0;i<files.length;i++)
-		{
-			var file=files[i];
-			if(dependencies[file]!==true)
-			{
-				require(path.join("..",file));
-				dependencies[file]=true;
-			}
-		}
-	}
+		require(path.join("..",moduleRegister[key]));
 	return oldGetModule(key);
 };
