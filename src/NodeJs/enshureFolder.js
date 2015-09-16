c0274c6c3563eb5a0b2a71b0ef1786c7e78020cc
Morgas <morgas01@web.de>
2015-09-16 22:35:46 +0200

    removed debug code

--- src/NodeJs/enshureFolder.js
+++ src/NodeJs/enshureFolder.js
@@ -4,14 +4,8 @@ var joinPath=path.join.apply.bind(path.join,path);
 
 module.exports=function(folderpath)
 {
-	console.log(folderpath);
 	var parts=folderpath.split(/[/\\]/);
 	var i=parts.length+1;
-	console.log(joinPath(parts.slice(0,i-1)),fs.existsSync(joinPath(parts.slice(0,i-1))));
-	while(i>1&&!fs.existsSync(joinPath(parts.slice(0,i-1))))
-	{
-		i--;
-		console.log(joinPath(parts.slice(0,i-1)),fs.existsSync(joinPath(parts.slice(0,i-1))));
-	}
+	while(i>1&&!fs.existsSync(joinPath(parts.slice(0,i-1)))) i--;
 	for(;i<=parts.length;i++) fs.mkdirSync(joinPath(parts.slice(0,i)));
 };
