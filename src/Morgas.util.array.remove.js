04e5f8516616db48c3a7f29338fd8804ae15ee5d
Morgas <morgas01@web.de>
2017-07-30 11:10:18 +0200

    created Morgas.util.array.remove

---
+++ src/Morgas.util.array.remove.js
@@ -0,0 +1,31 @@
+(function(µ,SMOD,GMOD,HMOD,SC){
+
+	var util=µ.util=µ.util||{};
+	var utilArray=util.array=util.array||{};
+
+	utilArray.remove=function(array,item)
+	{
+		var index=array.indexOf(item);
+		if(index!=-1) array.splice(index,1);
+		return index;
+	};
+	SMOD("array.remove",utilArray.remove);
+
+	utilArray.removeIf=function(array,predicate,scope=null,all=false)
+	{
+		if(all)
+		{
+			for(var i=array.length-1;i>=0;i--)
+			{
+				if(predicate.call(scope,array[i],i,array)) array.splice(i,1);
+			}
+		}
+		else
+		{
+			var index=array.findIndex(predicate,scope);
+			if(index!=-1) array.splice(index,1);
+		}
+	};
+	SMOD("array.removeIf",utilArray.removeIf);
+
+})(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
