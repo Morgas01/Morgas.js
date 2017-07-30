54b127b989d8a5643231f7495630e624b2d41444
Morgas <morgas01@web.de>
2017-07-30 11:09:46 +0200

    console signature for fun

---
+++ src/Morgas.signature.js
@@ -0,0 +1,17 @@
+(function(µ,SMOD,GMOD,HMOD,SC){
+
+	//SC=SC({});
+
+	µ.signature=
+`
+          _       .-.
+ _   _   (_)___   \\  |.-.
+| | | |  | / __|   \\_|_/
+| |_| |_ | \\__ \\   \`
+| ._,_(_)/ |___/ Contribute or Complain at
+|_|    |__/ https://github.com/Morgas01/Morgas.js
+`;
+
+	GMOD("signature",µ.signature);
+
+})(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
