73fb760dc9d45ba3807d2923e7d847ff33469936
Morgas <morgas01@web.de>
2019-04-15 10:04:55 +0200

    build

--- build/Morgas-0.8.8.js
+++ build/Morgas-0.8.8.js
@@ -996,6 +996,103 @@
 })(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
 /********************/
 (function(µ,SMOD,GMOD,HMOD,SC){
+
+	//SC=SC({});
+
+	/**
+	 * Transmutes data between versions
+	 */
+	let Transmuter=µ.Transmuter=µ.Class({
+		/**
+		 * {Object.<String,Object.<String,Function>>} (transmutations) - map of transmutes; mapping indicates {fromVersion:{toVersion:transmutation}}
+		 * {Function} (versionDetector) - returns version of single argument
+		 * {String} (currentVersion)
+		 */
+		constructor:function({transmutations={},versionDetector,currentVersion})
+		{
+			this.transmutations=transmutations;
+			this.versionDetector=versionDetector;
+			this.currentVersion=currentVersion;
+		},
+		addTransmutation(from,to,transmutation)
+		{
+			if(!this.transmutations[from])this.transmutations[from]={};
+			this.transmutations[from][to]=transmutation;
+		},
+		getTransmutation(from,to)
+		{
+			if(!this.transmutations[from]) return undefined;
+			return this.transmutations[from][to];
+		},
+		async transmute(from,to,data)
+		{
+			let transmutation=this.getTransmutation(from,to);
+			if(transmutation)
+			{
+				return await transmutation.call(this,data);
+			}
+			let path=this.lookUp(from,to);
+			for(let i=0;i<path.length-1;i++)
+			{
+				let from=path[i],to=path[i+1];
+				let step=this.getTransmutation(from,to);
+				data=await step.call(this,data);
+				if(data==null) µ.logger.warn(`#Transmuter:001 data became ${data} in step ${from}=>${to}`);
+			}
+			return data;
+		},
+		lookUp(from,to)
+		{
+			if(!this.transmutations[from]) throw new RangeError("#Transmuter:002 no transmutations for version "+from);
+			let todo=[[from]];
+			let shortestPath=null;
+			while(todo.length>0)
+			{
+				let path=todo.shift();
+				let current=path[path.length-1];
+				if(!this.transmutations[current]) continue;
+				if(this.transmutations[current][to])
+			 	{
+			 		path.push(to);
+			 		if(!shortestPath||shortestPath.length>path.length)
+			 		{
+			 			shortestPath=path;
+			 		}
+			 		continue;
+			 	}
+			 	for(let nextStep of Object.keys(this.transmutations[current]))
+			 	{
+			 		if(path.includes(nextStep)) continue; // do not cycle
+			 		todo.push(path.concat(nextStep));
+			 	}
+			}
+			if(!shortestPath) throw new RangeError("#Transmuter:003 no path between versions "+from+" and "+to);
+			let transmutationSteps=[];
+			return shortestPath;
+		},
+		async transmuteTo(to,data)
+		{
+			if(!this.versionDetector) throw new ReferenceError("#Transmuter:004 no versionDetector defined");
+			let from=await this.versionDetector(data);
+			return await this.transmute(from,to,data);
+		},
+		async transmuteFrom(from,data)
+		{
+			if(this.currentVersion==undefined) throw new ReferenceError("#Transmuter:005 no currentVersion defined");
+			return await this.transmute(from,this.currentVersion,data);
+		},
+		async transmuteCurrent(from,data)
+		{
+			if(this.currentVersion==undefined) throw new ReferenceError("#Transmuter:006 no currentVersion defined");
+			return await this.transmuteTo(this.currentVersion,data);
+		}
+	});
+
+	SMOD("Transmuter",Transmuter);
+
+})(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
+/********************/
+(function(µ,SMOD,GMOD,HMOD,SC){
 	
 	let util=µ.util=µ.util||{};
 
@@ -1880,7 +1977,7 @@
                 case "s":
                 	return leftPad(date.getSeconds(),2);
                 case "t":
-                	return leftPad(date.getMilliseconds(),2);
+                	return leftPad(date.getMilliseconds(),3);
                 case "z":
                 	return date.getTimezoneOffset()/60;
 			}
@@ -1934,9 +2031,9 @@
 	 *
 	 * @param {Function} fn
 	 * @param {Number} (delay=50) - time to group calls in ms
-	 * @param {Number} (maxDelay) - max time to group calls in ms
+	 * @param {Number} (maxDelay=delay*3) - max time to group calls in ms
 	 */
-	uFn.group=function(fn,delay=50,maxDelay)
+	uFn.group=function(fn,delay=50,maxDelay=delay*3)
 	{
 		let timer=null;
 		let maxTimer=null;
@@ -2688,7 +2785,7 @@
 		return todo[0].siblingResults[0];
 	};
 
-	NODE.traverseTo=function(root,path,childrenGetter)
+	NODE.traverseTo=function(root,path,{childrenGetter}={})
 	{
 		childrenGetter=NODE.normalizeChildrenGetter(childrenGetter);
 		if(typeof path=="string") path=path.split(".");
@@ -4113,7 +4210,7 @@
 	/**
 	 *
 	 * @param {Number} (stageCount=1) - count of generated stages
-	 * @param {Function} (mapType=WeakMap())
+	 * @param {Function} (mapType=Map())
 	 * @param {Function} (defaultValue=()=>new mapType())
 	 */
 	let register=uMap.register=function(stageCount,mapType=Map,defaultValue=()=>new mapType())
--- src/NodeJs/File.js
+++ src/NodeJs/File.js
@@ -1,33 +1,39 @@
 (function(µ,SMOD,GMOD,HMOD,SC){
 
-	µ.NodeJs=µ.NodeJs||{};
+	µ.NodeJs = µ.NodeJs || {};
 
-	SC=SC({
-		prom:"Promise"
+	SC = SC({
+		prom: "Promise"
 	});
-	
-	let FS=require("fs");
-	let PATH=require("path");
-	
-	let asyncCallback=function(signal)
+
+	let FS = require("fs");
+	let PATH = require("path");
+
+	let asyncCallback = function (signal)
 	{
-		return function(err,result)
+		return function (err, result)
 		{
-			if(err) signal.reject(err);
-			else signal.resolve(result);
+			if (err)
+			{
+				signal.reject(err);
+			}
+			else
+			{
+				signal.resolve(result);
+			}
 		};
 	};
-	let copyFile=function(source,target)
+	let copyFile = function (source, target)
 	{
-		return source.readStream().then(function(r)
+		return source.readStream().then(function (r)
 		{
-			return target.writeStream().then(function(w)
+			return target.writeStream().then(function (w)
 			{
-				return new SC.prom(function(signal)
+				return new SC.prom(function (signal)
 				{
-					r.on("error",signal.reject);
-					w.on("error",signal.reject);
-					w.on("close",function()
+					r.on("error", signal.reject);
+					w.on("error", signal.reject);
+					w.on("close", function ()
 					{
 						signal.resolve(target);
 					});
@@ -36,187 +42,191 @@
 			});
 		});
 	};
-	
-	let FILE=µ.NodeJs.File=µ.Class({
-		constructor:function(filePath)
+
+	let FILE = µ.NodeJs.File = µ.Class({
+		constructor: function (filePath)
 		{
-			this.filePath=filePath||".";
-			SC.prom.pledgeAll(this,["realPath","stat","lstat","access","listFiles","mkdir","read","readStream","write","writeStream","rename","copy"]);
+			this.filePath = filePath || ".";
+			SC.prom.pledgeAll(this, ["realPath", "stat", "lstat", "access", "listFiles", "mkdir", "read", "readStream", "write", "writeStream", "rename", "copy"]);
 		},
-		getFileName:function()
+		getFileName: function ()
 		{
 			return PATH.parse(this.filePath).name;
 		},
 		/** FileName+Ext **/
-		getName:function()
+		getName: function ()
 		{
 			return PATH.parse(this.filePath).base;
 		},
-		getDir:function()
+		getDir: function ()
 		{
 			return PATH.parse(this.filePath).dir;
 		},
-		getExt:function()
+		getExt: function ()
 		{
 			return PATH.parse(this.filePath).ext;
 		},
-		changePath:function(...path)
+		changePath: function (...path)
 		{
-			if(path instanceof FILE) path=path.getAbsolutePath();
-			this.filePath=PATH.resolve(this.filePath,...path);
+			if (path instanceof FILE) path = path.getAbsolutePath();
+			this.filePath = PATH.resolve(this.filePath, ...path);
 			return this;
 		},
-		getAbsolutePath:function()
+		getAbsolutePath: function ()
 		{
-			return this.filePath=PATH.resolve(this.filePath);
+			return this.filePath = PATH.resolve(this.filePath);
 		},
-		realPath:function(signal)
+		realPath: function (signal)
 		{
-			FS.realpath(this.filePath,asyncCallback(signal));
+			FS.realpath(this.filePath, asyncCallback(signal));
 		},
-		stat:function(signal)
+		stat: function (signal)
 		{
-			FS.stat(this.filePath,asyncCallback(signal));
+			FS.stat(this.filePath, asyncCallback(signal));
 		},
-		lstat:function(signal)
+		lstat: function (signal)
 		{
-			FS.lstat(this.filePath,asyncCallback(signal));
+			FS.lstat(this.filePath, asyncCallback(signal));
 		},
-		access:function(signal,mode)
+		access: function (signal, mode)
 		{
-			FS.access(this.filePath,mode,asyncCallback(signal));
+			FS.access(this.filePath, mode, asyncCallback(signal));
 		},
-		exists:function()
+		exists: function ()
 		{
 			return this.access(FS.F_OK);
 		},
-		listFiles:function(signal)
+		listFiles: function (signal)
 		{
-			FS.readdir(this.filePath,asyncCallback(signal));
+			FS.readdir(this.filePath, asyncCallback(signal));
 		},
-		mkdir:function(signal,dir,mode)
+		mkdir: function (signal, dir, mode)
 		{
-			FS.mkdir(PATH.join(this.filePath,dir),mode,asyncCallback(signal));
+			FS.mkdir(PATH.join(this.filePath, dir), mode, asyncCallback(signal));
 		},
-		read:function(signal,options)
+		read: function (signal, options)
 		{
-			FS.readFile(this.filePath,options,asyncCallback(signal));
+			FS.readFile(this.filePath, options, asyncCallback(signal));
 		},
-		readStream:function(signal,options)
+		readStream: function (signal, options)
 		{
-			FS.createReadStream(this.filePath,options)
-			.on("error",signal.reject)
-			.on("open",function()
+			FS.createReadStream(this.filePath, options)
+				.on("error", signal.reject)
+				.on("open", function ()
+				{
+					this.removeListener("error", signal.reject);
+					signal.resolve(this);
+				});
+		},
+		write: function (signal, data, options)
+		{
+			FS.writeFile(this.filePath, data, options, asyncCallback(signal));
+		},
+		writeStream: function (signal, options)
+		{
+			FS.createWriteStream(this.filePath, options)
+				.on("error", signal.reject)
+				.on("open", function ()
+				{
+					this.removeListener("error", signal.reject);
+					signal.resolve(this);
+				});
+		},
+		rename: function (signal, filename, overwrite)
+		{
+			let filePath = PATH.resolve(PATH.dirname(this.filePath), filename);
+			let doRename = () => FS.rename(this.filePath, filePath, (err, result) =>
 			{
-				this.removeListener("error",signal.reject);
-				signal.resolve(this);
-			});
-		},
-		write:function(signal,data,options)
-		{
-			FS.writeFile(this.filePath,data,options,asyncCallback(signal));
-		},
-		writeStream:function(signal,options)
-		{
-			FS.createWriteStream(this.filePath,options)
-			.on("error",signal.reject)
-			.on("open",function()
-			{
-				this.removeListener("error",signal.reject);
-				signal.resolve(this);
-			});
-		},
-		rename:function(signal,filename,overwrite)
-		{
-			let filePath=PATH.resolve(PATH.dirname(this.filePath),filename);
-			let doRename=()=>FS.rename(this.filePath,filePath,(err,result)=>
-			{
-				if(err) signal.reject(err);
+				if (err)
+				{
+					signal.reject(err);
+				}
 				else
 				{
-					this.filePath=filePath;
+					this.filePath = filePath;
 					signal.resolve(result);
 				}
 			});
-			if(overwrite===true)
+			if (overwrite === true)
 			{
 				doRename();
 			}
 			else
 			{
-				new FILE(filePath).exists().reverse("FILE_EXISTS",doRename).catch(signal.reject);
+				new FILE(filePath).exists().reverse("FILE_EXISTS", doRename).catch(signal.reject);
 			}
 		},
-		move:function(dir,overwrite)
+		move: function (dir, overwrite)
 		{
-			let target=PATH.join(FILE.fileToString(dir),PATH.basename(this.filePath));
-			return this.rename(target,overwrite);
+			let target = PATH.join(FILE.fileToString(dir), PATH.basename(this.filePath));
+			return this.rename(target, overwrite);
 		},
-		remove:function()
+		remove: function ()
 		{
-			return this.stat().then(function(stat)
+			return this.stat().then(function (stat)
 			{
-				if(stat.isDirectory())
+				if (stat.isDirectory())
 				{
-					return this.listFiles().then(function(files)
+					return this.listFiles().then(function (files)
 					{
-						return Promise.all(files.map(f=>this.clone().changePath(f).remove()))
-						.then(()=>new SC.prom(signal=>
-						{
-							FS.unlink(this.filePath,asyncCallback(signal))
-						}));
+						return Promise.all(files.map(f => this.clone().changePath(f).remove()))
+							.then(() => new SC.prom(signal =>
+							{
+								FS.unlink(this.filePath, asyncCallback(signal));
+							}));
 					});
 				}
 				else return new SC.prom(signal=>
-				{
-					FS.unlink(this.filePath,asyncCallback(signal));
-				});
-			},µ.constantFunctions.ndef);
+					{
+						FS.unlink(this.filePath, asyncCallback(signal));
+					});
+			}, µ.constantFunctions.ndef);
 		},
-		copy:function(signal,filename,overwrite)
+		copy: function (signal, filename, overwrite)
 		{
-			filename=FILE.fileToString(filename);
-			let filePath=PATH.resolve(PATH.parse(this.filePath).dir,filename);
+			filename = FILE.fileToString(filename);
+			let filePath = PATH.resolve(PATH.parse(this.filePath).dir, filename);
 			if(overwrite===true) copyFile(this,new FILE(filePath)).then(signal.resolve,signal.reject);
 			else new FILE(filePath).exists().reverse("FILE_EXISTS",function()
-			{
-				return copyFile(signal.scope,this);
-			}).then(signal.resolve,signal.reject);
+				{
+					return copyFile(signal.scope, this);
+				}).then(signal.resolve, signal.reject);
 		},
-		copyToDir:function(dir,overwrite)
+		copyToDir: function (dir, overwrite)
 		{
-			dir=FILE.fileToString(dir);
-			return this.copy(PATH.join(dir,PATH.basename(this.filePath)),overwrite);
+			dir = FILE.fileToString(dir);
+			return this.copy(PATH.join(dir, PATH.basename(this.filePath)), overwrite);
 		},
-		clone:function()
+		clone: function ()
 		{
 			return new FILE(this.filePath);
 		},
-		toString:function()
+		toString: function ()
 		{
-			return "[FILE: "+this.filePath+"]";
+			return "[FILE: " + this.filePath + "]";
 		},
-		equals:function(other)
+		equals: function (other)
 		{
-			return (other instanceof FILE) && other.getAbsolutePath()==this.getAbsolutePath();
+			return (other instanceof FILE) && other.getAbsolutePath() === this.getAbsolutePath();
 		}
 	});
-	FILE.stringToFile=function(path)
+	FILE.isFile = function (file)
 	{
-		if(path instanceof FILE) return path;
-		return new FILE(path);
+		return file instanceof FILE;
 	};
-	FILE.fileToString=function(file)
+	FILE.stringToFile = function (path)
 	{
-		if(file instanceof FILE) return file.filePath;
-		return file;
+		return FILE.isFile(path) ? path : new FILE(path);
 	};
-	FILE.fileToAbsoluteString=function(file)
+	FILE.fileToString = function (file)
 	{
-		if(file instanceof FILE) return file.getAbsolutePath();
-		return PATH.resolve(file);
+		return FILE.isFile(file) ? file.filePath : file;
 	};
-	
-	SMOD("File",FILE);
-	
-})(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
+	FILE.fileToAbsoluteString = function (file)
+	{
+		return FILE.isFile(file) ? file.getAbsolutePath() : PATH.resolve(file);
+	};
+
+	SMOD("File", FILE);
+
+})(Morgas, Morgas.setModule, Morgas.getModule, Morgas.hasModule, Morgas.shortcut);
